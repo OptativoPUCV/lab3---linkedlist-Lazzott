bf837a233639fde2fa4033696bfb7d5b5728c5d6
Your Name <you@example.com>
2024-03-27 20:27:29 +0000

    new try

--- list.c
+++ list.c
@@ -38,9 +38,12 @@ List * createList()
 
 void * firstList(List * list)
 {
-    if(list==NULL) return NULL;
+    if (list == NULL)
+        return NULL;
+    if (list->head == NULL)
+        return NULL;
+    list->current = list->head;
     return list->head->data;
-    return NULL;
 }
 
 void * nextList(List * list) 
